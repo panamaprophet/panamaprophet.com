aa68d603478089875ced61604dc71c073e8dafdf
pp <pp@rawsound.ru>
2022-04-22 16:50:09 +0200

    removed destructuring in api/stream. just because

--- pages/api/stream/[id].ts
+++ pages/api/stream/[id].ts
@@ -2,11 +2,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { resolveStreamUrl } from '../../../resolvers/soundcloud';
 
 
-const handler = async (request: NextApiRequest, response: NextApiResponse) => {
-    const { query } = request;
-    const { id } = query;
-
-    const url = await resolveStreamUrl({ id: Number(id) });
+const handler = async ({ query }: NextApiRequest, response: NextApiResponse) => {
+    const url = await resolveStreamUrl({ id: Number(query.id) });
 
     response.status(200).json({ url });
 };
