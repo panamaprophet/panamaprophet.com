05e1bf31ce48e55864f0aeaaad43427f3f9841e3
pp <pp@rawsound.ru>
2023-01-27 03:08:17 +0100

    upload images to s3

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ module.exports = {
         CLIENT_ID: process.env.CLIENT_ID,
         CLIENT_SECRET: process.env.CLIENT_SECRET,
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 's3.eu-west-1.amazonaws.com',
+                port: '',
+                pathname: '/panamaprophet.com/**',
+            },
+        ],
+    },
 };
